f737f99b482cc3c932b98febcb4253be3e27d7bb
Utkarsh Khambra <utkarsh.khambra@itiviti.com>
2023-10-07 19:41:33 +0530

    WIP counting primes

--- src/cpp/helpers/primes.cpp
+++ src/cpp/helpers/primes.cpp
@@ -83,15 +83,12 @@ std::vector<std::invoke_result_t<F, R>>
 reduce_combinations(R &&r, size_t m, F &&reduce) noexcept {
   using return_t = std::invoke_result_t<F, R>;
   std::vector<return_t> ret;
-  // std::cout << "Fuck";
   auto func = [&](auto filter) {
     size_t index = 0;
     auto combination =
         r | std::views::filter([&](auto const &i) {
           return *std::ranges::next(std::ranges::begin(filter), index++);
         });
-    for (auto i : combination)
-      index = 0;
     ret.push_back(reduce(combination));
   };
   cool_lex(r.size() - m, m, func);
@@ -101,42 +98,35 @@ reduce_combinations(R &&r, size_t m, F &&reduce) noexcept {
 // template <typename sieve>
 // requires(std::invocable<sieve, size_t>)
 inline size_t legendre_formula(size_t n) noexcept {
-  assert(n > 20);
-  auto primes = sieve_of_eratosthens(std::ceil(std::sqrt(n)));
+  // assert(n > 20);
+  auto primes = sieve_of_eratosthens(std::floor(std::sqrt(n)) + 1);
   auto mult_reduce = [](auto r) -> size_t {
     size_t val = 1;
     for (auto i : r) {
-      std::cout << "Fuck " << i << " ";
       val *= i;
     }
-    std::cout << "\n";
     return val;
   };
   auto sum = primes.size() + n;
   for (auto i : std::views::iota(size_t{1}, primes.size() + 1)) {
     auto f = reduce_combinations(primes, i, mult_reduce);
-    for (auto &j : f) {
+    for (auto j : f) {
+      // std::cout << j << " ";
       j = static_cast<size_t>(std::floor(static_cast<double>(n) / j));
-      std::cout << j << " ";
-      sum += j * -1 * (i % 2);
+      // std::cout << j << " ";
+      sum += j * (i % 2 ? -1 : 1);
+      // std::cout << sum << " " << j << " ";
+      // std::cout << sum << " ";
     }
-    std::cout << "\n ";
+    // std::cout << "\n ";
   }
-  return sum;
+  return sum - 1;
 }
 
 inline size_t meissel_lehmer(size_t n) noexcept {}
 
 int main() {
-  std::vector<char> s{'a', 'b', 'c', 'd', 'f'};
-  auto id = [](char c) { std::cout << c; };
-  // for_each_combination(s, 3, id);
-  // cool_lex(1, 2);
-  auto pi_n = legendre_formula(30);
-  std::cout << "pi_n " << pi_n;
-  // for_each_combination(s, 5, id);
-
-  // auto j = sieve_of_eratosthens(std::floor(std::sqrt(30)));
-  // for (auto i : j)
-  //   std::cout << i << "\n";
+  // std::cout  << legendre_formula(14745) << "\n";
+  for (auto i : std::views::iota(1, 40))
+    std::cout << i << " " << legendre_formula(i) << "\n";
 }
